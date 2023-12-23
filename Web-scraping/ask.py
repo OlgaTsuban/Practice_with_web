8010100c13f702f167d0834c26ae16a3152d76d6
Olga Tsuban <119211067+OlgaTsuban@users.noreply.github.com>
2023-12-23 23:23:17 +0200

    add web-scraping

---
+++ Web-scraping/ask.py
@@ -0,0 +1,73 @@
+from typing import List, Any
+import redis
+from redis_lru import RedisLRU
+from models import Author, Quote
+from mongoengine import connect
+from pymongo import MongoClient
+
+# Information for connection
+clients = redis.StrictRedis(host="127.0.0.1", port=6379, password=None)
+cache = RedisLRU(clients)
+uri = "mongodb+srv://tsubanolga:O1904@cluster0.yiefpq6.mongodb.net/Cluster0?retryWrites=true&w=majority"
+
+# Establish a connection to MongoDB
+connect(host=uri)
+
+# Create a new client and connect to the server
+client = MongoClient(uri)
+db = client.Cluster0
+
+# Functon can find information using the tags , using cache
+@cache
+def find_by_tag(tag: str) -> list[str | None]:
+    print(f"Find by {tag}")
+    quotes = Quote.objects(tags__iregex=tag)
+    result = [q.quote for q in quotes]
+
+    # keys = clients.keys('*')
+    # print(keys)
+
+    return result
+
+# Functon can find inform using authors , using cache
+@cache
+def find_by_author(author: str) -> list[list[Any]]:
+    print(f"Find by {author}")
+    authors = Author.objects(fullname__iregex=author)
+    result = {}
+    for a in authors:
+        quotes = Quote.objects(author=a)
+        result[a.fullname] = [q.quote for q in quotes]
+    return result
+
+# Main function, has all logic
+def main():
+    while True:
+        try:
+            args_str = input("Enter command and value (e.g., name: John): ")
+            if args_str == "exit":
+                print("Exiting the script.")
+                break
+            args_list = args_str.split(":")
+            command = args_list[0].strip()
+            value = args_list[1].strip()
+            if command == "name":
+                result = find_by_author(value)
+            elif command == "tag":
+                result = find_by_tag(value)
+            elif command == "tags":
+                # Split the comma-separated tags
+                tags = value.split(",")
+                result = {tag: find_by_tag(tag) for tag in tags}
+            else:
+                print("Invalid command.")
+                continue
+
+            print(result)
+        except Exception as e:
+            print(f"An error occurred: {str(e)}")
+
+
+if __name__ == '__main__':
+    main()
+    
---
+++ Web-scraping/seed.py
@@ -0,0 +1,46 @@
+import json
+from mongoengine.errors import NotUniqueError
+from models import Author, Quote
+from pymongo import MongoClient
+from mongoengine import connect
+
+uri = "mongodb+srv://tsubanolga:O1904@cluster0.yiefpq6.mongodb.net/Cluster0?retryWrites=true&w=majority"
+
+# Establish a connection to MongoDB
+connect(host=uri)
+
+# Create a new client and connect to the server
+client = MongoClient(uri)
+db = client.Cluster0
+
+# Send a ping to confirm a successful connection 
+# Is not necessary part of program
+# try:
+#     client.admin.command('ping')
+#     print("Pinged your deployment. You successfully connected to MongoDB!")
+# except Exception as e:
+#     print(e)
+
+# Get data from files .json
+def import_data_from_json(json_file, model_class, related_model=None, related_field=None):
+    with open(json_file, encoding='utf-8') as fd:
+        data = json.load(fd)
+        for el in data:
+            try:
+                # Create an instance of the main model class
+                instance = model_class(**el)
+
+                # If there is a related model and field specified, try to associate them
+                if related_model and related_field:
+                    related_obj, *_ = related_model.objects(fullname=el.get(related_field))
+                    setattr(instance, related_field, related_obj)
+
+                instance.save()
+            except NotUniqueError:
+                print(f"{model_class.__name__} already exists: {el.get('fullname')}")
+
+if __name__ == '__main__':
+        # Example usage for authors
+    import_data_from_json('authors.json', Author)
+        # Example usage for quotes with related author
+    import_data_from_json('quotes.json', Quote, related_model=Author, related_field='author')
